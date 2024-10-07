d8f8a5e66fabefaecfe0adc6dbd0b388dd001ff4
Renato Vicencio N <renatovicencion@gmail.com>
2024-10-06 22:43:49 -0300

    :sparkles: feat: tasks and projects crud

---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,108 @@
+import { Router } from "express";
+import { body, param } from "express-validator";
+import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middlewares/validation";
+import { TaskController } from "../controllers/TaskController";
+import { projectExists } from "../middlewares/project";
+import { taskBelongsToProject, taskExists } from "../middlewares/task";
+const router = Router();
+
+router.post(
+  "/",
+  body("projectName")
+    .notEmpty()
+    .withMessage("El nombre del proyecto es obligatorio."),
+  body("clientName")
+    .notEmpty()
+    .withMessage("El nombre del cliente es obligatorio."),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripción del proyecto es obligatoria."),
+  handleInputErrors,
+  ProjectController.createProject
+);
+
+router.get("/", ProjectController.getAllProjects);
+
+router.get(
+  "/:id",
+  param("id").isMongoId().withMessage("ID no válido."),
+  handleInputErrors,
+  ProjectController.getProjectById
+);
+
+router.put(
+  "/:id",
+  param("id").isMongoId().withMessage("ID no válido."),
+  body("projectName")
+    .notEmpty()
+    .withMessage("El nombre del proyecto es obligatorio."),
+  body("clientName")
+    .notEmpty()
+    .withMessage("El nombre del cliente es obligatorio."),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripción del proyecto es obligatoria."),
+  handleInputErrors,
+  ProjectController.updateProject
+);
+
+router.delete(
+  "/:id",
+  param("id").isMongoId().withMessage("ID no válido."),
+  handleInputErrors,
+  ProjectController.deleteProject
+);
+
+/* Routes for tasks */
+router.param("projectId", projectExists);
+
+router.post(
+  "/:projectId/tasks",
+  body("name").notEmpty().withMessage("El nombre de la tarea es obligatoria."),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripción de la tarea es obligatoria."),
+  handleInputErrors,
+  TaskController.createTask
+);
+
+router.get("/:projectId/tasks", TaskController.getProjectTasks);
+
+router.param("taskId", taskExists);
+router.param("taskId", taskBelongsToProject);
+
+router.get(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("ID no válido."),
+  handleInputErrors,
+  TaskController.getTaskById
+);
+
+router.put(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("ID no válido."),
+  body("name").notEmpty().withMessage("El nombre de la tarea es obligatoria."),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripción de la tarea es obligatoria."),
+  handleInputErrors,
+  TaskController.updateTask
+);
+
+router.delete(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("ID no válido."),
+  handleInputErrors,
+  TaskController.deleteTask
+);
+
+router.post(
+  "/:projectId/tasks/:taskId/status",
+  param("taskId").isMongoId().withMessage("ID no válido."),
+  body("status").notEmpty().withMessage("El estado es obligatorio."),
+  handleInputErrors,
+  TaskController.updateTaskStatus
+);
+
+export default router;
